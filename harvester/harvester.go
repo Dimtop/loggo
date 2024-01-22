17e1bc0e7a0809d53ed5b8dfbe8a508fc05c7dbd
Dimtop <topalidisdofcl@gmail.com>
2024-01-22 12:09:18 +0200

    adds harvester handling functions

--- harvester/harvester.go
+++ harvester/harvester.go
@@ -1,22 +1,24 @@
 package harvester
 
 import (
-	"fmt"
 	"loggo/config"
 	"loggo/file"
+	"loggo/log"
 	"loggo/utils"
 	"time"
 )
 
 type Harvester struct {
-	Ticker     *time.Ticker
-	ConfigPath string
+	Ticker            *time.Ticker
+	ConfigPath        string
+	OnLogsParsedFuncs []func(logs []log.Log)
 }
 
-func InitializeHarvester(configPath string) *Harvester {
+func InitializeHarvester(configPath string, onLogsParsedFuncs []func(logs []log.Log)) *Harvester {
 	h := &Harvester{
-		Ticker:     time.NewTicker(time.Second * 5),
-		ConfigPath: configPath,
+		Ticker:            time.NewTicker(time.Second * 5),
+		ConfigPath:        configPath,
+		OnLogsParsedFuncs: onLogsParsedFuncs,
 	}
 
 	return h
@@ -29,9 +31,18 @@ func (h *Harvester) Run() {
 		case <-h.Ticker.C:
 
 			cfg := config.ReadConfigFile(h.ConfigPath)
-			logs := file.ParseFile(cfg.LastUsedFile, cfg.SeekPos)
-			fmt.Println(logs)
+			var logs []log.Log
+			logsAsStrings := file.ParseFile(cfg.LastUsedFile, cfg.SeekPos)
+
+			for _, logStr := range logsAsStrings {
+				logs = append(logs, log.ParseLogFromString(logStr))
+			}
+
 			cfg.UpdateConfig(h.ConfigPath, cfg.EmitChangeSeekposMessage(h.ConfigPath, utils.GetFileSize(cfg.LastUsedFile)))
+
+			for _, function := range h.OnLogsParsedFuncs {
+				function(logs)
+			}
 
 		}
 	}
--- main.go
+++ main.go
@@ -1,8 +1,20 @@
 package main
 
-import "loggo/harvester"
+import (
+	"fmt"
+	"loggo/harvester"
+	"loggo/log"
+)
 
 func main() {
-	h := harvester.InitializeHarvester("config.json")
+	printFunc := func(logs []log.Log) {
+		fmt.Println(logs)
+	}
+	printLengthFunc := func(logs []log.Log) {
+		fmt.Println(len(logs))
+	}
+	funcs := []func(logs []log.Log){printFunc, printLengthFunc}
+
+	h := harvester.InitializeHarvester("C:\\Users\\topal\\Desktop\\Code\\Orderit\\tools\\logs\\config.json", funcs)
 	h.Run()
 }
